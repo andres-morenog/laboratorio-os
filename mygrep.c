d5fb14a1975cd89d8524cc80dc8542c0e5dbc4b7
Andres <andres.morenog@udea.edu.co>
2018-05-07 00:56:57 +0000

    Added mygrep

---
+++ mygrep.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+    FILE *ptr_file;
+    char buf[1000];
+	ptr_file =fopen(argv[2],"r");
+	if (!ptr_file)
+    	return 1;
+	while (fgets(buf,1000, ptr_file)!=NULL){
+	    if (strstr(buf, argv[1]) != NULL) {
+             printf("%s",buf);
+        }
+	}
+	fclose(ptr_file);
+    return 0; 
+}
